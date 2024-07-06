27bc241cf1c888ee7ca6b7eb3754d6c6d996fc77
Muhammad Usman <131748967+Musman391@users.noreply.github.com>
2024-07-06 17:47:24 -0600

    Wrote the login of the readChar() function that updates the position of the character

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "go/token"
+
 type Lexer struct {
 	input        string
 	position     int  // current position in input (points to current char)
@@ -9,5 +11,25 @@ type Lexer struct {
 
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
+	l.readChar()
 	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	}
 }
