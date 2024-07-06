93e4eb32349ad9b8334fc0eb14cf9da4dbaea73c
Muhammad Usman <131748967+Musman391@users.noreply.github.com>
2024-07-06 17:58:03 -0600

    Wrote the nextToken() function and the newToken, NextTokenreturns the next token by calling new token after matching the correct token

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,6 +1,6 @@
 package lexer
 
-import "go/token"
+import "Monkey-Interpreter/token"
 
 type Lexer struct {
 	input        string
@@ -31,5 +31,29 @@ func (l *Lexer) NextToken() token.Token {
 	switch l.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case '0':
+		tok.Literal = ""
+		tok.Type = token.EOF
 	}
+
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
