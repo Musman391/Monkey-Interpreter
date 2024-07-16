2908eec0f248acd9355b0459d46b19fb0927ff6d
Muhammad Usman <131748967+Musman391@users.noreply.github.com>
2024-07-15 20:50:21 -0600

    Created the parseIntegerLiteral so we can parse the integer

--- parser/parser.go
+++ parser/parser.go
@@ -5,6 +5,7 @@ import (
 	"Monkey-Interpreter/lexer"
 	"Monkey-Interpreter/token"
 	"fmt"
+	"strconv"
 )
 
 const (
@@ -171,4 +172,18 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 
 func (p *Parser) parseIdentifier() ast.Expression {
 	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.curToken}
+
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as intger", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+	lit.Value = value
+
+	return lit
 }
