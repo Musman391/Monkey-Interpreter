060936dad95f2838a67568cdb6f403516a87072d
Muhammad Usman <131748967+Musman391@users.noreply.github.com>
2024-07-22 14:34:13 -0600

    Created the parseCallExpression that calls a helper function to help parse the call expression the helper function returns a list of expressions and the call expression takes in an expression that it will parse

--- parser/parser.go
+++ parser/parser.go
@@ -62,6 +62,7 @@ func New(l *lexer.Lexer) *Parser {
 
 	p.registerPrefix(token.FUNCTION, p.parseFunctionLiteral)
 
+	p.registerInfix(token.LPAREN, p.parseCallExpression)
 	return p
 }
 
@@ -390,4 +391,34 @@ func (p *Parser) parseFunctionParameters() []*ast.Identifier {
 
 	return identifiers
 
+}
+
+func (p *Parser) parseCallExpression(function ast.Expression) ast.Expression {
+	exp := &ast.CallExpression{Token: p.curToken, Function: function}
+	exp.Arguments = p.parseCallArguments()
+	return exp
+}
+
+func (p *Parser) parseCallArguments() []ast.Expression {
+	args := []ast.Expression{}
+
+	if p.peekTokenIs(token.RPAREN) {
+		p.nextToken()
+		return args
+	}
+
+	p.nextToken()
+	args = append(args, p.parseExpression(LOWEST))
+
+	for p.peekTokenIs(token.COMMA) {
+		p.nextToken()
+		p.nextToken()
+		args = append(args, p.parseExpression(LOWEST))
+	}
+
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+
+	return args
 }
