7398bb26974e8de4d166e52b438833b1c582b307
Muhammad Usman <131748967+Musman391@users.noreply.github.com>
2024-07-25 21:28:47 -0600

    Created a new function called evalStatements which is called when we have the top of the node the starting point, and we need to traverse the whole tree by calling Eval with each statement in the tree

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -7,9 +7,24 @@ import (
 
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
+	// Statements
+	case *ast.Program:
+		return evalStatements(node.Statements)
+
+	// Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 	}
 
 	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+
+	return result
 }
